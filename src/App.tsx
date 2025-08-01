818e2f68865861d3aca5519eb0a5cfb1433f1988
kooldeep-27 <rajputkooldeep@gmail.com>
2025-08-01 21:47:34 +0530

    removed admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CaseStudyPage from './pages/CaseStudyPage';
 import WebsitesPage from './pages/WebsitesPage';
 import WhatsAppFloat from './components/WhatsAppFloat';
-import AdminApp from './admin/AdminApp';
 
 function AppWrapper() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
 
   return (
     <div className="min-h-screen bg-white">
@@ -16,9 +13,8 @@ function AppWrapper() {
         <Route path="/" element={<HomePage />} />
         <Route path="/case-study" element={<CaseStudyPage />} />
         <Route path="/websites" element={<WebsitesPage />} />
-        <Route path="/admin/*" element={<AdminApp />} />
       </Routes>
-      {!isAdminRoute && <WhatsAppFloat />}
+      <WhatsAppFloat />
     </div>
   );
 }
